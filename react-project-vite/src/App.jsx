e5582f15d0eec5056eaaff56be2726b6a5535460
sdgamer007 <sohaildua38@gmail.com>
2023-11-07 02:40:51 +0100

    adding elements clearing and reset functionalities

--- react-project-vite/src/App.jsx
+++ react-project-vite/src/App.jsx
@@ -2,10 +2,11 @@ import React, { useState } from "react"
 import { NameFunc } from "./NameFunc"
 import { Counter } from "./Counter"
 
+const INTIAL_VALUE = ["A", "B", "C"]
 function App() {
   const [name, setName] = useState("Bulbasaur")
   // 1. create a new array to store 3 values
-  const [array, setArray] = useState(["A", "B", "C"])
+  const [array, setArray] = useState(INTIAL_VALUE)
   /*return (
     <div>
       <NameFunc name="hola" age={21} />
@@ -32,12 +33,44 @@ function App() {
       return currentArray.filter((el) => el !== letter)
     })
   }
+
+  function addLetterToEnd(letter) {
+    setArray((currentArray) => {
+      return [...currentArray, letter]
+    })
+  }
+
+  function addLetterToStart(letter) {
+    setArray((currentArray) => {
+      return [letter, ...currentArray]
+    })
+  }
+
+  function clear() {
+    setArray((currentArray) => {
+      return []
+    })
+  }
+
+  function reset() {
+    setArray(INTIAL_VALUE)
+  }
+
   return (
     <div>
       <button onClick={removeFirstElement}>Remove</button>
       <button onClick={() => removeSpecificElement("B")}>
         Specific Element
       </button>
+      <button onClick={() => addLetterToEnd("B")}>
+        Specific Element to end
+      </button>
+      <button onClick={() => addLetterToStart("1")}>
+        Specific Element to start
+      </button>
+      <button onClick={clear}>clear</button>
+      <button onClick={reset}>reset</button>
+
       {array.join(",")}
     </div>
   )
