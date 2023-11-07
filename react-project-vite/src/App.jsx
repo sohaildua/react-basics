02c7e36cabbf6778b4a319004e37accf38969061
sdgamer007 <sohaildua38@gmail.com>
2023-11-07 01:16:00 +0100

    remove first element

--- react-project-vite/src/App.jsx
+++ react-project-vite/src/App.jsx
@@ -4,13 +4,15 @@ import { Counter } from "./Counter"
 
 function App() {
   const [name, setName] = useState("Bulbasaur")
+  // 1. create a new array to store 3 values
+  const [array, setArray] = useState(["A", "B", "C"])
   /*return (
     <div>
       <NameFunc name="hola" age={21} />
       <Counter></Counter>
     </div>
   )*/
-  return (
+  /*return (
     <input
       type="text"
       value={name}
@@ -18,6 +20,18 @@ function App() {
         setName(e.target.value)
       }}
     />
+  )*/
+  //
+  function removeFirstElement() {
+    setArray((currentArray) => {
+      return currentArray.splice(1)
+    })
+  }
+  return (
+    <div>
+      <button onClick={removeFirstElement}>Remove</button>
+      {array.join(",")}
+    </div>
   )
 }
 
