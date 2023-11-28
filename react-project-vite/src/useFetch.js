7741ca453a41335de2d0f2296ca571abf8e38400
sdgamer007 <sohaildua38@gmail.com>
2023-11-28 01:45:13 +0100

    abort controller worked and cancel done

--- react-project-vite/src/useFetch.js
+++ react-project-vite/src/useFetch.js
@@ -10,19 +10,26 @@ export function useFetch(url, options = {}) {
     setData(undefined)
     setIsError(false)
     setIsLoading(true)
+    const abortController = new AbortController()
 
     axios
-      .get(url)
+      .get(url, { signal: abortController.signal })
       .then((response) => {
         setData(response.data.filter((user) => user.id === 1))
         console.log("Success")
       })
       .catch((error) => {
+        if (error.name === "AbortError") return
         setIsError(true)
       })
       .finally(() => {
+        if (abortController.signal.aborted) return
         setIsLoading(false)
       })
+
+    return () => {
+      abortController.abort()
+    }
   }, [url])
 
   return { data, isError, isLoading }
